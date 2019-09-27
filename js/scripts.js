5fd7ea12c40fc6d6e1944fb4aa86d95da61fd9a9
Natalia Vega <nvega@argencode.com>
2019-09-27 19:39:51 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.galeria .contenedor-imagen').on('click', function(){
+        $('#modal').modal;
+        var ruta_imagen = ($(this).find('img').attr('src'));
+        $('#imagen-modal').attr('src', ruta_imagen);
+    });
+})
