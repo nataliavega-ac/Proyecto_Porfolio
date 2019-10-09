7e8e5980a9d46c166c85b5fa7e8c76fed2aa8276
Natalia Vega <nvega@argencode.com>
2019-10-08 23:15:46 -0300

    Boton para ir arriba
    Definición del boton para ir arriba del todo

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,29 @@ $(function(){
     });
 
 
+
+    // Botón para ir arriba
+    $('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		},300 );
+	});
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 0){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		};
+	});
+
+
+
+
+
+
+
+
     // Acciones del formulario de consultas
     $("#boton_formulario").click(function(){
         //console.log("El botón ha sido presionado.");
