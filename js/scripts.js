a02fd052923c1a3ee813ebe943c695719cea91a1
Natalia Vega <nvega@argencode.com>
2019-11-08 11:10:47 -0300

    Formulario
    Mejoras en formulario

--- js/scripts.js
+++ js/scripts.js
@@ -26,43 +26,75 @@ $(function(){
 	});
 
 
+    // Destraba el validador
+    $("#nombre").keyup(function() {
+        $("#nombre").removeClass("borde_alerta").addClass("borde_normal");
+    });
+    $("#email").keyup(function() {
+        $("#email").removeClass("borde_alerta").addClass("borde_normal");
+    });
 
     // Acciones del formulario de consultas
-    $("#boton_formulario").click(function(){
-        console.log("El botón ha sido presionado.");
+    $("#boton_formulario").click(function(event){
+       
+        // Previene el envío accidental del formulario
+        event.preventDefault();
+        event.stopPropagation();
 
         // Declaración de variables
         let nombre = $("#nombre").val();
         let email = $("#email").val();
         let mensaje = $("#mensaje").val();
 
-        console.log("Nombre: " + nombre + " Email: " + email + " Mensaje: " + mensaje );
-
-        // Variables para pasarle al procesador del formulario procesa_formulario.php
-        var parametros = {
-            "nombre": nombre,
-            "email": email,
-            "mensaje": mensaje
+        var validate = true;
+        
+        if(email.indexOf('@', 0) == -1 || email.indexOf('.', 0) == -1) {
+            $.notify("Debe ingresar un email válido", "error");
+            $("#email").removeClass("borde_normal").addClass("borde_alerta");
+            $("#email").focus();
+            validate = false;
         };
-    
-        // Llamada Ajax
-        // Esto es lo que llama al formulario
-    
-        $.ajax({
-            type: 'POST',
-            dataType:'JSON',
-            url: 'procesa_formulario.php',
-            data: parametros,
-            success:function(data){
-                $.notify("Formulario enviado con éxito.", "success");
-                $("#nombre").val("");
-                $("#email").val("");
-                $("#mensaje").val("");
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                $.notify("Existió un error en el envío del mail", "error");
-            }
-        });
+
+        if (nombre.length < 2) {
+            $.notify("Debe completar el nombre y debe tener 3 o más caracteres", "error");
+            $("#nombre").removeClass("borde_normal").addClass("borde_alerta");
+            $("#nombre").focus();
+            validate = false;
+        };
+
+
+        if(validate) {
+
+            // Variables para pasarle al procesador del formulario procesa_formulario.php
+            var parametros = {
+                "nombre": nombre,
+                "email": email,
+                "mensaje": mensaje
+            };
+
+            // Llamada Ajax
+            // Esto es lo que llama al formulario
+
+            $.ajax({
+                type: 'POST',
+                dataType:'JSON',
+                url: 'procesa_formulario.php',
+                data: parametros,
+                success:function(data){
+                    $.notify("Formulario enviado con éxito.", "success");
+                    $("#nombre").val("");
+                    $("#email").val("");
+                    $("#mensaje").val("");
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    $.notify("Existió un error en el envío del mail", "error");
+                }
+            });
+
+
+        };
+
+       
 
     });
 
